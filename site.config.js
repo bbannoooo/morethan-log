630a5596bd29121b648de74907defaa5ab156d19
b4no <63397370+bbannoooo@users.noreply.github.com>
2024-04-19 11:31:30 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    // linkedin: "morethanmin",
+    email: "bbannoooo@gmail.com",
+    linkedin: "bbannoooo",
     github: "bbannoooo",
     instagram: "",
   },
